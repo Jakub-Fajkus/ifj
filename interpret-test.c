20468128eef34b88ce638f7c7605cdc8c874dd64
Andrej <anhucko297@gmail.com>
2016-11-24 18:34:39 +0100

    interpret: testing list of instructions vol.2

--- interpret-test.c
+++ interpret-test.c
@@ -3,6 +3,7 @@
 //
 
 #include "Interpret.h"
+#include "BasicStructures.h"
 
 void callInterpret() {
 
@@ -20,13 +21,38 @@ void callInterpret() {
     ListInsertLast(TestInstructionList, createUpcomingInstruction(createFirstInstruction()));
 
     ListFirst(TestInstructionList);
-    printInstructionTest(1,TestInstructionList);
+    //printInstructionTest(1,TestInstructionList);
 
 
     // Inserting instruction "Create Global Variable Without Value"
     ListInsertLast(TestInstructionList, createUpcomingInstruction(createGlobalVariable("Main.x", TYPE_INT)));
+    //printf("-------------------------- fuck you guys --------------------");
+    //printf("\n----- %s", (char *)TestInstructionList->Last->element.data.instr->address_dst);
+    //printf("\n----- %d", *(DATA_TYPE*)TestInstructionList->Last->element.data.instr->address_src1);
+
+
     ListSuccessor(TestInstructionList);
-    printInstructionTest(2,TestInstructionList);
+    //printInstructionTest(2,TestInstructionList);
+
+    VARIABLE_VALUE insertValue;
+    insertValue.doubleValue = 4.2;
+    ListInsertLast(TestInstructionList, createUpcomingInstruction(pushGlobalVariable("Main.a", TYPE_DOUBLE, insertValue)));
+
+
+
+    ListSuccessor(TestInstructionList);
+    //printInstructionTest(3,TestInstructionList);
+
+    printf("-------------------------- fuck you guys --------------------");
+    printf("\n----- %s", (char *)TestInstructionList->Last->element.data.instr->address_dst);
+    printf("\n----- %d", *(DATA_TYPE*)TestInstructionList->Last->element.data.instr->address_src1);
+    printf("\n----- %g\n", *(VARIABLE_VALUE*)TestInstructionList->Last->element.data.instr->address_src2);
+
+
+    printf("\n----- LAST TEST: Print All Instructions.\n");
+    printListOfInstructions(TestInstructionList);
+
+
 }
 
 
@@ -47,5 +73,17 @@ void printInstructionTest(int x, tDLList *TestInstructionList) {
     printf("Ptr Src1 Addr: %p\n", TestInstructionList->Act->element.data.instr->address_src1);
     printf("Ptr Src2 Addr: %p\n", TestInstructionList->Act->element.data.instr->address_src2);
     printf("----- TEST %d OK ----- ----- ----- ----- ----- ----- ----- -----\n",x);
+
+}
+
+void printListOfInstructions(tDLList *TestInstructionList) {
+
+    int number=1;
+    ListFirst(TestInstructionList);
+    do {
+        printf("Instruction number %d: %d\n", number, TestInstructionList->Act->element.data.instr->type);
+        ListSuccessor(TestInstructionList);
+    } while( TestInstructionList->Act != NULL );
+
 
 }
