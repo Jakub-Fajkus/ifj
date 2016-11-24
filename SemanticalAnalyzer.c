b8373c5c7e9d2d7a5cd02319d500cc14d4a9fc28
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-11-24 17:51:59 +0100

    SemanticalAnalyzer with global variables

--- SemanticalAnalyzer.c
+++ SemanticalAnalyzer.c
@@ -3,3 +3,8 @@
 //
 
 #include "SemanticalAnalyzer.h"
+#include "SymbolTable.h"
+
+SYMBOL_TABLE_NODEPtr *globalSymbolTable;
+SYMBOL_TABLE_NODEPtr *actualSymbolTable;
+SYMBOL_TABLE_FUNCTION *actualFunction;
