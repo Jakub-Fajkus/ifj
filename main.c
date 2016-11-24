a6448c01471776d36bd6ff40662cdbe3b856a1b6
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-11-24 15:58:53 +0100

    Enable syntactical analysis in main.c

--- main.c
+++ main.c
@@ -20,14 +20,15 @@ int main(int argc, char *argv[])
     // uncomment what you need
 
 
-    simulateSymbolTable();
+//    simulateSymbolTable();
 /*
     if (argc != 2) {
         printf("no input file found\n");
         return 99;
     }
+    */
     runSyntacticalAnalysis(argv[1]);
-     */
+
 
 //    callInterpret();    // #justInterpretThings
 
