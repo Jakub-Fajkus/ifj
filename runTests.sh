b8b5c6606c68fb732dff3f1c166b7544eb623aa3
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-12-10 13:52:59 +0100

    Update runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 FILES=`find tests -iname  'test*' | sort -n | grep -e txt -e ifj16`
-FILES_without_read=`find tests -not -path "*tests/tests-7*" -iname  'test*' | sort -n | grep -e txt -e ifj16`
+FILES_without_read=`find tests -iname  'test*' | sort -n | grep -e txt -e ifj16`
 FAILED_COUNTER=0
 OK_COUNTER=0
+FAILED_TESTS=()
 
 for FILE in $FILES_without_read; do
     EXPECTED_RETURN_CODE=`echo $FILE | cut -d'/' -f 2 | cut -d'-' -f 2`
@@ -18,10 +19,12 @@ for FILE in $FILES_without_read; do
     else
         printf "\e[31mexpected %d, got %d -> test FAILED\e[m" "$EXPECTED_RETURN_CODE" "$RETURNED_CODE"
         ((FAILED_COUNTER++))
+        FAILED_TESTS+=("$FILE")
     fi
-
 done
 
 printf "\e[34m\n\n\n**************** END OF TESTS ****************\n"
 printf "\e[34mtests ok: %d \n\e[m" "$OK_COUNTER"
 printf "\e[31mtests failed: %d \n\e[m" "$FAILED_COUNTER"
+
+echo "${FAILED_TESTS[@]}"
